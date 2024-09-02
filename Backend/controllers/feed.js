53797fd1f60c55b40ed84a0a2974ff9d7962a1ec
Saul Pereira <saul_pereira@hotmail.com>
2024-09-02 17:41:16 -0600

    Adding a Server Side Validation
    Adding a Server Side Validation

--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [
@@ -16,12 +18,26 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res
+            .status(422)
+            .json({
+                message: 'Validation failed, entered data is incorrect.',
+                errors: errors.array()
+            });
+    }
     const title = req.body.title;
     const content = req.body.content;
     console.log(title, content);
     //Create Post in db
     res.status(201).json({
         message: 'Post created successfully',
-        post: {id: new Date().toISOString(), title: title, content: content}
+        post: {
+            _id: new Date().toISOString(), 
+            title: title, 
+            content: content, 
+            creator: { name: 'Saul'}},
+            createdAt: new Date()
     });
 };
