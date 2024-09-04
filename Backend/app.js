74039e395a573eecaf7ae603a97a1f2fdfcc86d2
Saul Pereira <saul_pereira@hotmail.com>
2024-09-04 11:20:55 -0600

    380. Static Images & Error Handling
    380. Static Images & Error Handling

--- Backend/app.js
+++ Backend/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -7,6 +9,7 @@ const feedRoutes = require('./routes/feed');
 const app = express();
 
 app.use(bodyParser.json()); // application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,6 +19,13 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({message: message});
+})
 
 mongoose.connect(
     'mongodb+srv://saulp:rxNJ4yait8SS2Pj@cluster0.qqwo7oa.mongodb.net/messages'
